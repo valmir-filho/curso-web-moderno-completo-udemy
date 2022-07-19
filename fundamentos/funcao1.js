f4fb18450abd37ef8c3d8ddb322c784b5fd814ab
Valmir <vmcfilho@gmail.com>
2022-07-19 14:49:24 -0300

    Add files via upload

---
+++ fundamentos/funcao1.js
@@ -0,0 +1,10 @@
+// Função sem retorno.
+function imprimirSoma(a, b) {
+    console.log(a + b);
+}
+imprimirSoma(2, 3);
+// Função com retorno.
+function soma(a, b) {
+    return a + b;
+}
+console.log(soma(2, 3));
