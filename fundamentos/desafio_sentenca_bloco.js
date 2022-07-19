6286fc7cdd9b6906e3926381f0fbb364f85ee69a
Valmir <vmcfilho@gmail.com>
2022-07-19 09:03:47 -0300

    Add files via upload

---
+++ fundamentos/desafio_sentenca_bloco.js
@@ -0,0 +1,17 @@
+// Bloco 1: Imprime os números ímpares entre 0 e 10.
+{
+    console.log(1);
+    console.log(3);
+    console.log(5);
+    console.log(7);
+    console.log(9);
+}
+// Bloco 2: Imprime os números pares entre 0 e 10.
+{
+    console.log(0);
+    console.log(2);
+    console.log(4);
+    console.log(6);
+    console.log(8);
+    console.log(10);
+}
