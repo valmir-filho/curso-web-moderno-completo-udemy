7f200af1e3888e19dca161240e68842b6493e0fc
Valmir <vmcfilho@gmail.com>
2022-07-19 09:21:23 -0300

    Add files via upload

---
+++ fundamentos/dados_1.js
@@ -0,0 +1,11 @@
+var nome = "Caneta";
+var quantidade = 10;
+var preco = 6.4;
+var imposto = 1.5;
+var precoFinal = preco + imposto;
+
+console.log("Nome: " + nome + ".");
+console.log("Quantidade: " + quantidade + " unidades.");
+console.log("Preço: R$" + preco + ".");
+console.log("Imposto: R$" + imposto + ".");
+console.log("Preço Final: R$" + precoFinal + ".");
