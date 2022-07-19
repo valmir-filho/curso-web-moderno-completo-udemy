dda6ac0d6c24698c02673561c47198a233ba0361
Valmir <vmcfilho@gmail.com>
2022-07-19 13:24:50 -0300

    Add files via upload

---
+++ fundamentos/booleanos.js
@@ -0,0 +1,16 @@
+let isAtivo = false;
+
+// Verdadeiros.
+console.log(isAtivo);
+console.log(!!3);
+console.log(!!-1);
+console.log(!!" ");
+console.log(!![]);
+console.log(!!{});
+console.log(!!Infinity);
+// Falsos.
+console.log(!!0);
+console.log(!!"");
+console.log(!!null);
+console.log(!!NaN);
+console.log(!!undefined);
