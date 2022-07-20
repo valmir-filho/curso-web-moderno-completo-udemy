53075ea1933639de420cca85e58bb5b2c8a695a4
Valmir <vmcfilho@gmail.com>
2022-07-20 14:16:58 -0300

    Add files via upload

---
+++ fundamentos/atribuicao.js
@@ -0,0 +1,12 @@
+const A = 7;
+let b = 3;
+b += A;  // Atribuição aditiva (b = b + a).
+console.log(b);
+b -= 4;  // Atribuição subtrativa (b = b - 4).
+console.log(b);
+b *= 2;  // Atribuição multiplicativa (b = b * 2).
+console.log(b);
+b /= 2;  // Atribuição divisiva (b = b / 2).
+console.log(b);
+b %= 2;  // Atribuição modular (b = b % 2).
+console.log(b);
