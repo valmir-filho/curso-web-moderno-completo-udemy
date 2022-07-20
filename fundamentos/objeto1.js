6ee54b61d3e8a8f7987ac9c6d003c6aa1d8e4ef4
Valmir <vmcfilho@gmail.com>
2022-07-20 11:49:56 -0300

    Add files via upload

---
+++ fundamentos/objeto1.js
@@ -0,0 +1,4 @@
+const PROD1 = {};  // Comando para criação de um objeto (coleção de chave e valor). É similar a um dicionário.
+PROD1.nome = "Celular";  // Comando para inclusão no objeto.
+PROD1.preco = 4985.4;
+console.log(PROD1);
