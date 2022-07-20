337c21136d49f7a9cbdfdbda2da3cbafa297b729
Valmir <vmcfilho@gmail.com>
2022-07-20 13:16:54 -0300

    Add files via upload

---
+++ fundamentos/objeto2.js
@@ -0,0 +1,8 @@
+console.log(typeof Object);
+console.log(typeof new Object());
+const CLIENTE = function() {};
+    console.log(typeof CLIENTE);
+    console.log(typeof new CLIENTE);
+    class Produto {};
+    console.log(typeof Produto);
+    console.log(typeof new Produto());
