f53696c7285e88daf3065b693437ff7bdd9d6fe8
Valmir <vmcfilho@gmail.com>
2022-07-21 10:47:36 -0300

    Add files via upload

---
+++ fundamentos/desafioSentencaBloco.js
@@ -0,0 +1,17 @@
+// Bloco 1: Impressão dos números ímpares entre 0 e 10.
+{
+    console.log(1);
+    console.log(3);
+    console.log(5);
+    console.log(7);
+    console.log(9);
+}
+// Bloco 2: Impressão dos números pares entre 0 e 10.
+{
+    console.log(0);
+    console.log(2);
+    console.log(4);
+    console.log(6);
+    console.log(8);
+    console.log(10);
+}
