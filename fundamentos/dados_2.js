a7f64dc1fea0dcc225e6fd83697a1eb43ff9d864
Valmir <vmcfilho@gmail.com>
2022-07-21 10:48:17 -0300

    Delete dados_2.js

--- fundamentos/dados_2.js
+++
@@ -1,6 +0,0 @@
-let preco = 19.9;
-let desconto = 0.4;
-let precoComDesconto = preco * (1 - desconto);
-console.log("Preço: R$" + preco + ".");
-console.log("Desconto: R$" + desconto + ".");
-console.log("Preço com Desconto: R$" + precoComDesconto + ".");
