2f7aaf1b6a1f323251b5ed59a1abc84c0d7254d5
Valmir <vmcfilho@gmail.com>
2022-07-22 09:06:29 -0300

    Add files via upload

--- estruturas-controle/doWhile.js
+++ estruturas-controle/doWhile.js
@@ -2,7 +2,7 @@ function getInteiroAleatorioEntre(min, max) {
     const valor = Math.random() * (max - min) + min;
     return Math.floor(valor);
 }
-let opcao = -1;
+let opcao = -1;  // A declaração dessa variável, no comando "do while" torna-se opcional, pois ele executará pelo menos uma vez.
 // O comando "do while" executará pelo menos uma vez, mesmo que a condição seja atendida no início.
 do {
     opcao = getInteiroAleatorioEntre(-1, 10);
