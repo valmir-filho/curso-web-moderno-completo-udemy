dc76b3f5f1ec4fccd56aae75b6645e9e03e8716d
Valmir <vmcfilho@gmail.com>
2022-07-23 19:32:26 -0300

    Add files via upload

---
+++ exercícios/2.js
@@ -0,0 +1,19 @@
+/* Os triângulos podem ser classificados em 3 tipos, quanto ao tamanho de seus lados:
+- Equilátero: Três lados iguais. 
+- Isósceles: Dois lados iguais.
+- Escaleno: Todos os lados diferentes.
+Crie uma função que receba o valor dos três lados de um triângulo e retorne sua classificação. (abstraia as condições matemáticas de existência de um triângulo). */
+function vericaTriangulo(a, b, c) {
+    if(a == b && b == c) {
+        console.log("Triângulo Equilátero");
+    } else if(a == b || a == c || b == c) {
+        console.log("Triângulo Isóceles");
+    } else {
+        console.log("Triângulo Escaleno");
+    }
+}
+vericaTriangulo(2, 2, 2);
+vericaTriangulo(2, 2, 3);
+vericaTriangulo(2, 3, 2);
+vericaTriangulo(3, 2, 2);
+vericaTriangulo(1, 2, 3);
