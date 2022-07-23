2277415cde74df814139096615c37e1ac083cad4
Valmir <vmcfilho@gmail.com>
2022-07-23 19:16:48 -0300

    Add files via upload

---
+++ exercícios/1.js
@@ -0,0 +1,6 @@
+/* Crie uma função que dado dois valores (passados como parâmetros) mostre no console a soma, a subtração, a multiplicação e 
+a divisão desses valores. */
+function calculadora(x, y) {
+    console.log(x + y, x - y, x * y, x / y);
+}
+calculadora(8, 4);
