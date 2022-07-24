cfd0f16e0144fe302d2e71eddcff75e5468b4553
Valmir <vmcfilho@gmail.com>
2022-07-24 08:57:41 -0300

    Update 5.js

--- exercícios/5.js
+++ exercícios/5.js
@@ -2,6 +2,7 @@
 "console.log(0.1 + 0.2);"? O resultado será: 0.30000000000000004. Outra coisa importante de se observar, é o fato de que o ponto é utilizado no lugar
 da vírgula e vice versa. Com isso, faça um exercício para mostrar um valor monetário sempre da forma correta. Desenvolva uma função que receba o valor
 R$0.30000000000000004 e retorne R$0,30. */
+
 function formatarValorDecimal(valorDecimal) {
     valorEmReais = `R$${valorDecimal.toFixed(2).toString().replace(".", ",")}`;
     console.log(valorEmReais);
