43cb1311feefa6d21017c9fe86c88577cdc04ae9
Valmir <vmcfilho@gmail.com>
2022-07-24 08:57:07 -0300

    Update 6.js

--- exercícios/6.js
+++ exercícios/6.js
@@ -1,4 +1,6 @@
-/* Elabore duas funções que recebam três parâmetros: capital inicial, taxa de juros e tempo de aplicação. A primeira função retornará o montante da aplicação financeira sob o regime de juros simples e a segunda retornará o valor da aplicação sob o regime de juros compostos. */
+/* Elabore duas funções que recebam três parâmetros: capital inicial, taxa de juros e tempo de aplicação. A primeira função retornará o montante da
+aplicação financeira sob o regime de juros simples e a segunda retornará o valor da aplicação sob o regime de juros compostos. */
+
 function jurosSimples(capitalInicial, taxaJuros, tempoAplicacao) {
     let calculoJurosSimples = (capitalInicial * (1 + taxaJuros * tempoAplicacao)).toFixed(2);
     console.log(`Montante (juros simples) = R$${calculoJurosSimples}`);
