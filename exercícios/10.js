3e603a0d6be0c8f72156afb445fcb35e97df1fab
Valmir <vmcfilho@gmail.com>
2022-07-24 11:12:55 -0300

    Update 10.js

--- exercícios/10.js
+++ exercícios/10.js
@@ -1,4 +1,7 @@
-/* Um homem decidiu ir à uma revenda comprar um carro. Ele deseja comprar um carro hatch, e a revenda possui, além de carros hatch, sedans, motocicletas e caminhonetes. Utilizando uma estrutura switch, caso o comprador queira o hatch, retorne: “Compra efetuada com sucesso”. Nas outras opções, retorne: “Tem certeza que não prefere este modelo?”. Caso seja especificado um modelo que não está disponível, retorne no console: “Não trabalhamos com este tipo de meio de transporte”. */
+/* Um homem decidiu ir à uma revenda comprar um carro. Ele deseja comprar um carro hatch, e a revenda possui, além de carros hatch, sedans, motocicletas
+e caminhonetes. Utilizando uma estrutura switch, caso o comprador queira o hatch, retorne: “Compra efetuada com sucesso”. Nas outras opções, retorne:
+“Tem certeza que não prefere este modelo?”. Caso seja especificado um modelo que não está disponível, retorne no console: “Não trabalhamos com este tipo
+de meio de transporte”. */
 function escolherCarro(modelo) {
     switch(modelo) {
         case "hatch":
