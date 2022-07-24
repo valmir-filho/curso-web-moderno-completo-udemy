bbfa3643b40d36b26fabf42f46fe0f8d6f5c3257
Valmir <vmcfilho@gmail.com>
2022-07-24 10:48:33 -0300

    Add files via upload

---
+++ exercícios/8.js
@@ -0,0 +1,30 @@
+/* Crie um programa que exiba se um dia é útil, final de semana, ou inválido, dado o número referente. Considere que domingo é o dia 1 e sábado é o dia 7. Utilize a estrutura Switch. */
+function verificaDia(dia) {
+    switch(dia) {
+        case 1:
+            console.log(`Dia ${dia}: Domingo é final de semana.`);
+            break;
+        case 2:
+            console.log(`Dia ${dia}: Segunda-feira é dia útil.`);
+            break;
+        case 3:
+            console.log(`Dia ${dia}: Terça-feira é dia útil.`);
+            break;
+        case 4:
+            console.log(`Dia ${dia}: Quarta-feira é dia útil.`);
+            break;
+        case 5:
+            console.log(`Dia ${dia}: Quinta-feira é dia útil.`);
+            break;
+        case 6:
+            console.log(`Dia ${dia}: Sexta-feira é dia útil.`);
+            break;
+        case 7:
+            console.log(`Dia ${dia}: Sábado é final de semana.`);
+            break;
+        default:
+            console.log("Dia inválido!");
+            break;
+    }
+}
+verificaDia(1);
