870f929a16a795a51b2ac2bcbc323681e7cbc78a
Valmir <vmcfilho@gmail.com>
2022-07-24 11:00:30 -0300

    Update 9.js

--- exercícios/9.js
+++ exercícios/9.js
@@ -1,4 +1,6 @@
-/* Crie uma estrutura condicional switch que receba uma string com o nome de uma fruta e que possua três casos: Caso maçã, retorne no console: “Não vendemos esta fruta aqui”. Caso kiwi, retorne: “Estamos com escassez de kiwis”. Caso melancia, retorne: “Aqui está, são 3 reais o quilo”. Teste com estas três opções. Crie também um default, que retornará uma mensagem de erro no console. */
+/* Crie uma estrutura condicional switch que receba uma string com o nome de uma fruta e que possua três casos: Caso maçã, retorne no console:
+“Não vendemos esta fruta aqui”. Caso kiwi, retorne: “Estamos com escassez de kiwis”. Caso melancia, retorne: “Aqui está, são 3 reais o quilo”.
+Teste com estas três opções. Crie também um default, que retornará uma mensagem de erro no console. */
 function nomeFruta(fruta) {
     switch(fruta) {
         case "maçã":
