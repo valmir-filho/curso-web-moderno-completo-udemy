b36d4777816d7981eafa4d5cafc117f724827212
Valmir <vmcfilho@gmail.com>
2022-07-24 20:26:42 -0300

    Update 13.js

--- exercícios/13.js
+++ exercícios/13.js
@@ -1,4 +1,5 @@
-/* Faça um programa que leia um número entre 0 e 10, e escreva este número por extenso. Use o comando switch. Crie um case default que escreva "Número fora do intervalo." */
+/* Faça um programa que leia um número entre 0 e 10, e escreva este número por extenso. Use o comando switch. Crie um case default que escreva "Número
+fora do intervalo." */
 function numeroPorExtenso(numero) {
     switch(numero) {
         case 0:
