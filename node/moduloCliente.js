b01e596e201684af4c935b97c73efdff93b5bd5f
Valmir <vmcfilho@gmail.com>
2022-07-25 18:43:19 -0300

    Update moduloCliente.js

--- node/moduloCliente.js
+++ node/moduloCliente.js
@@ -1,5 +1,5 @@
-const moduloA = require("./moduloA");
-const moduloB = require("./moduloB");
+const moduloA = require("./moduloA.js");
+const moduloB = require("./moduloB.js");
 console.log(moduloA.ola);
 console.log(moduloA.bemVindo);
 console.log(moduloA.ateLogo);
