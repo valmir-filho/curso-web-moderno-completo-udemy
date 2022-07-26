d0b357364dddc4f65fb811268f992a13450b4332
Valmir <vmcfilho@gmail.com>
2022-07-26 15:34:36 -0300

    Add files via upload

---
+++ node/global.js
@@ -0,0 +1,6 @@
+global.MinhaApp = Object.freeze ({  // Comando "freeze" faz o congelamento.
+    saudacao() {
+        return "Estou em todos os lugares!";
+    },
+    nome: "Sistema legal."
+})
---
+++ node/globalCliente.js
@@ -0,0 +1,4 @@
+require("./global");
+console.log(MinhaApp.saudacao());
+MinhaApp.nome = "Eita!";
+console.log(MinhaApp.nome);
