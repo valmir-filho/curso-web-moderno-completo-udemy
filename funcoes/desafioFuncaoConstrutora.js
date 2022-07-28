f720d56017faef224aae27f21a4cbc35dd54d301
Valmir <vmcfilho@gmail.com>
2022-07-28 08:57:34 -0300

    Delete funcoes directory

--- funcoes/desafioFuncaoConstrutora.js
+++
@@ -1,8 +0,0 @@
-function Pessoa(nome) {
-    this.nome = nome;
-    this.falar = function() {
-        console.log(`Meu nome é ${this.nome}.`);
-    }
-}
-const p1 = new Pessoa("Valmir");
-p1.falar();
--- funcoes/thisEBind.js
+++
@@ -1,9 +0,0 @@
-const pessoa = {
-    saudacao: "Bom dia!",
-    falar() {
-        console.log(this.saudacao);
-    }
-}
-pessoa.falar();
-const falarDePessoa = pessoa.falar.bind(pessoa);
-falarDePessoa();
