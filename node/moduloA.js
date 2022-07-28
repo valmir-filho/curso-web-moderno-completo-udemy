627bc49f6356d5524f826c653acc410432a0c4f9
Valmir <vmcfilho@gmail.com>
2022-07-28 08:57:45 -0300

    Delete node directory

--- node/moduloA.js
+++
@@ -1,4 +0,0 @@
-// Em "node", um arquivo representa um módulo.
-this.ola = "Fala Pessoal";
-exports.bemVindo = "Bem vindo ao node!";
-module.exports.ateLogo = "Até o próximo exemplo";
--- node/usandoModulosTerceiros.js
+++
@@ -1,2 +0,0 @@
-const _ = require("lodash");  
-setInterval(() => console.log(_.random(1, 1000)), 2000);
